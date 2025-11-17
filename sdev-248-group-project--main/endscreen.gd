5f9dc3391bd8aa96c8cc8ef049ea87a2f29ed48e
sconwell10 <94002133+sconwell10@users.noreply.github.com>
2025-11-17 10:26:03 -0500

    Add files via upload
    programed in an intro cutscene and programed the end cutscene with out an end goal till we decide where we would like to place it

---
+++ sdev-248-group-project--main/endscreen.gd
@@ -0,0 +1,32 @@
+extends CanvasLayer
+
+@onready var button = $Button
+@onready var panel = $Panel
+@onready var player = get_parent().get_node("player")
+
+func show_victory():
+	#show victory screen and disable the player movement
+	panel.visible = true
+	button.visible = true
+	visible = true
+	if player:
+		player.can_move = false
+		
+	button.pressed.connect(_on_button_pressed)
+	
+func _on_button_pressed():
+	# hide victory screen and restart level
+	panel.visible = false
+	button.visible = false
+	visible = false
+	
+	if player:
+		player.can_move = true
+	#restart the level
+	get_tree().reload_current_scene()
+	
+
+
+func _on_goal_body_entered(body: Node2D) -> void:
+	if body.name == "player":
+		show_victory()
